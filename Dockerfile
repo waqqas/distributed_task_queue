3fed1dc7fe93797ac3132507fa9680b76b059cbc
Waqqas Jabbar <waqqas.jabbar@gmail.com>
2024-10-21 09:48:18 +0000

    WIP: Task producer app

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,19 @@
 FROM mcr.microsoft.com/devcontainers/java:1-21-bullseye
 
+COPY --from=golang:1.23-bullseye /usr/local/go/ /usr/local/go/
+ 
+ENV PATH="/usr/local/go/bin:${PATH}"
+
 ARG DEBIAN_FRONTEND=noninteractive
 ARG USER=vscode
 
 USER $USER
 ARG HOME="/home/$USER"
+
+RUN go install github.com/bazelbuild/buildtools/buildifier@latest
+RUN go install github.com/bazelbuild/buildtools/buildozer@latest
+RUN go install github.com/bazelbuild/buildtools/unused_deps@latest
+
+ENV PATH="$HOME/go/bin:${PATH}"
 
 RUN curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
--- services/task_producer/src/com/waqqasjabbar/dtq/task_producer/TaskProducerApp.java
+++ services/task_producer/src/com/waqqasjabbar/dtq/task_producer/TaskProducerApp.java
@@ -1,8 +1,42 @@
 package com.waqqasjabbar.dtq.task_producer;
 
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import io.github.cdimascio.dotenv.Dotenv;
+import com.waqqasjabbar.dtq.common.Task;
+
 public class TaskProducerApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello World from Task Producer!");
+
+        // Load environment variables from .env file
+        try {
+            Dotenv dotenv = Dotenv.load();
+            String kafkaHost = dotenv.get("KAFKA_HOST");
+            String kafkaPort = dotenv.get("KAFKA_PORT");
+            String kafkaBootstrapServers = kafkaHost + ":" + kafkaPort;
+            // String grpcHost = dotenv.get("GRPC_HOST");
+            // int grpcPort = Integer.parseInt(dotenv.get("GRPC_PORT"));
+
+            KafkaProducer<String, Task> kafkaProducer = new KafkaProducer<String, Task> (kafkaBootstrapServers);
+
+            TaskProducerService producer = new TaskProducerService(kafkaProducer, "");
+            producer.start();
+
+            java.util.Scanner scanner = new java.util.Scanner(System.in);
+            String userInput;
+            do {
+                System.out.print("Enter a task (type 'quit' to exit): ");
+                userInput = scanner.nextLine();
+                if (!userInput.equalsIgnoreCase("quit")) {
+                    producer.sendTask(userInput);
+                }
+            } while (!userInput.equalsIgnoreCase("quit"));
+            scanner.close();
+        } catch (Exception e) {
+            System.err.println("Error loading environment variables: " + e.getMessage());
+            System.exit(1);
+        }
+
     }
 }
